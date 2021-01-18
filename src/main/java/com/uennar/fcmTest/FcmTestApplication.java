3129f3016ce429ac16a7b1def3177338e55c1aee
Khalim Galimov <khalimgal@gmail.com>
2021-01-18 20:46:22 +0300

    Creating app for test fcm

---
+++ src/main/java/com/uennar/fcmTest/FcmTestApplication.java
@@ -0,0 +1,14 @@
+package com.uennar.fcmTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FcmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmTestApplication.class, args);
+	}
+
+}
